5ad3c2526348b7ee2350fbdf0175337dfe6551d0
JosafatV <josav09@gmail.com>
2016-11-15 21:21:53 -0600

    actualizacion a scripts
    Agrego a los scripts de llenado y sps necesarios, arreglo y adapto
    atributos de la tabla de creaciones. faltan pruebas y datos en los
    scripts de llenado

--- ScriptLlenado.sql
+++ ScriptLlenado.sql
@@ -1,6 +1,14 @@
 Use MyLearnDB;
 GO
 
+EXEC SP_Insertar_Universidad 'Instituto Tecnologico de Costa Rica'
+EXEC SP_Insertar_Universidad 'Universidad Nacional'
+EXEC SP_Insertar_Universidad 'Universidad de Costa Rica'
+
+EXEC SP_Insertar_Tecnologia 'SQL'
+EXEC SP_Insertar_Tecnologia 'HTML'
+EXEC SP_Insertar_Tecnologia 'C#'
+
 EXEC SP_Insertar_Estudiante '1', '1234', 'gsg4aer6g4ar68g46r5g', 'Drive', 'thrj4=56d3k', 'Gabriel', 'Angelos', '09-0873-0544', 'Gabriel@gmail.com', 85748231, 'Costa Rica', 'San Jose', '11/15/2016','GabrielA', 'rg38a4gr8a4r3g8a7r4g'
 EXEC SP_Insertar_Estudiante '2', '1234', '4adfn98g4ar65g4ar98n', 'Drive', '45gf3=l4h53', 'Nathaniel','Garro', '02-0623-0463', 'Nathaniel@gmail.com', 89843549, 'Costa Rica', 'San Jose', '11/15/2016','NathanielG', '8g34arg863a74rgarg38' 
 EXEC SP_Insertar_Estudiante '3', '1234', 'gt4ar64a664a6r8g34ar', 'Drive', 'k4563=kp4j3', 'Sebastian', 'Yarrick','05-0780-0642', 'Sebastian@gmail.com', 86548943, 'Costa Rica', 'Heredia', '11/15/2016','SebastianY', 'a4gr8a4r3g8a7r4g8a7r'
@@ -14,13 +22,13 @@ EXEC SP_Insertar_Estudiante '10', '1234', 'lk5g36f45jg3x54345yu', 'Drive', '4781
 EXEC SP_Insertar_Estudiante '11', '1234', 'l3g6ty3d45thrj456d3k', 'Drive', '878il=o34kg', 'Olga', 'Karamanz', '06-0723-0455', 'Olga@gmail.com', 84791285, 'Costa Rica', 'San Jose', '11/15/2016','OlgaK', '563kp4j3uiopbgny878c'
 EXEC SP_Insertar_Estudiante '12', '1234', '45gf3l4h53jk4563kp4j', 'Drive', 'hj83g=h7k8d', 'Katherine', 'Elysius', '06-0046-0240', 'Katherine@gmail.com', 86152783, 'Costa Rica', 'Heredia', '11/15/2016','KatherineE', 'r6789xs64a6r8g34arg8'
 
-EXEC SP_Insertar_Profesor '13','1234','f4gfjniglk5g36f45jg3','Drive','3g6ty=3d456','Ada','Lovelace','Ada@gmail.com',27658127,'11/15/2016','Lunes y Miercoles 9:30 a 12:30','Costa Rica','San Jose'
-EXEC SP_Insertar_Profesor '14', '1234','x54345yul3g6ty3d4563','Drive','3thrj=4564a','Blaise','Pascal','Blaise@gmail.com',22935176,'11/15/2016','Martes y Jueves 7:30 a 9:30','Costa Rica','San Jose'
-EXEC SP_Insertar_Profesor '15','1234','97u8lo74il853tx586h7','Drive','6r8g3=4arg8','Carl','Sagan','Carl@gmail.com',26250687,'11/15/2016','Miercoles y Viernes 13:00 a 15:00','Costa Rica','San Jose'
-EXEC SP_Insertar_Profesor '16','1234','s35h7ga863f4gfjniglk','Drive','63a74=rgarg','Heinrich','Hertz','Heinrich@gmail.com',22506892,'11/15/2016','Miercoles y Viernes 13:00 a 15:00','Costa Rica','San Jose'
-EXEC SP_Insertar_Profesor '17','1234','5g36f45jg3x54345yul3','Drive','38a4g=r8a4r','Gottfried','Leibniz','Gottfried@gmail.com',24565424,'11/15/2016','Lunes y Miercoles 9:30 a 12:30','Costa Rica','San Jose'
-EXEC SP_Insertar_Profesor '18','1234','g6ty3d4563thrj456d3k','Drive','3g8a7=r4g8a','Beatrix','Potter','Beatrix@gmail.com',25560462,'11/15/2016','Miercoles y Viernes 13:00 a 15:00','Costa Rica','San Jose'
-EXEC SP_Insertar_Profesor '19','1234','853tx586h7s35h7ga863','Drive','3x543=45yul','Edwin','Hubble','Edwin@gmail.com',27946215,'11/15/2016','Martes y Jueves 7:30 a 9:30','Costa Rica','Heredia'
+EXEC SP_Insertar_Profesor '13','1234','f4gfjniglk5g36f45jg3','Drive','3g6ty=3d456','Ada','Lovelace','Ada@gmail.com',27658127,'11/15/2016','Lunes y Miercoles 9:30 a 12:30','Costa Rica','San Jose', '1'
+EXEC SP_Insertar_Profesor '14', '1234','x54345yul3g6ty3d4563','Drive','3thrj=4564a','Blaise','Pascal','Blaise@gmail.com',22935176,'11/15/2016','Martes y Jueves 7:30 a 9:30','Costa Rica','San Jose', '1'
+EXEC SP_Insertar_Profesor '15','1234','97u8lo74il853tx586h7','Drive','6r8g3=4arg8','Carl','Sagan','Carl@gmail.com',26250687,'11/15/2016','Miercoles y Viernes 13:00 a 15:00','Costa Rica','San Jose', '1'
+EXEC SP_Insertar_Profesor '16','1234','s35h7ga863f4gfjniglk','Drive','63a74=rgarg','Heinrich','Hertz','Heinrich@gmail.com',22506892,'11/15/2016','Miercoles y Viernes 13:00 a 15:00','Costa Rica','San Jose', '1'
+EXEC SP_Insertar_Profesor '17','1234','5g36f45jg3x54345yul3','Drive','38a4g=r8a4r','Gottfried','Leibniz','Gottfried@gmail.com',24565424,'11/15/2016','Lunes y Miercoles 9:30 a 12:30','Costa Rica','San Jose', '1'
+EXEC SP_Insertar_Profesor '18','1234','g6ty3d4563thrj456d3k','Drive','3g8a7=r4g8a','Beatrix','Potter','Beatrix@gmail.com',25560462,'11/15/2016','Miercoles y Viernes 13:00 a 15:00','Costa Rica','San Jose', '1'
+EXEC SP_Insertar_Profesor '19','1234','853tx586h7s35h7ga863','Drive','3x543=45yul','Edwin','Hubble','Edwin@gmail.com',27946215,'11/15/2016','Martes y Jueves 7:30 a 9:30','Costa Rica','Heredia', '2'
 
 
 EXEC SP_Insertar_Empresa '20','1234','3uiopbgny878cr6789xs','Drive','3h4ar=8697u','Ernest','Rutherford','Marte','Marte41@gmail.com',25417489,'11/15/2016','http://warhammer40k.wikia.com/wiki/Mars','Costa Rica','San Jose','Marte41'											
@@ -36,3 +44,36 @@ EXEC SP_Insertar_Admin '27','1234','45gf3l4h53jk4563kp4j', '', '', 'Joseph','Cam
 EXEC SP_Insertar_Admin '28','1234','3uiopbgny878cr6789xs', '', '', 'Sebastian','Gonzalez'
 EXEC SP_Insertar_Admin '29','1234','3re6d3k45gf3l4h53jk4', '', '','Josafat','Vargas'
 EXEC SP_Insertar_Admin '30','1234','563kp4j3uiopbgny878c', '', '', 'Giovanni','Villalobos'
+
+
+INSERT INTO *****************************************************************************
+
+EXEC SP_Insertar_Curso '13', 'Especificacion y Diseño de Software', 'CE-4101', '1'
+
+/*CE-4101*/
+EXEC SP_Insertar_Badge 'Usabilidad', '25', '1'
+EXEC SP_Insertar_Badge 'Diagramas de Diseño', '25', '1'
+EXEC SP_Insertar_Badge 'Especificación de Proyectos', '25', '1'
+EXEC SP_Insertar_Badge 'Patrones de Diseño', '25', '1'
+
+
+EXEC SP_Insertar_Proyecto '1', 'DigiLearn', 'La ausencia de una plataforma para trabajar en línea y obtener experiencia laboral','ofrecer un sericio a estudiantes para que puedan trabajar en cursos de su universidad y en ofertas de trabajo', '1', '10/17/2016', '11/26/2016', '', 70
+
+EXEC SP_Insertar_Trabajo 'Decripción de STCs', 'Gran cantidad de informacion se encuentra en este STC, es necesario decriptarlo', '20', '11/10/2016', '12/01/2016', ''
+EXEC SP_Insertar_Trabajo 'DrPischel', 'Para ser más competitivo la farmacia DrPischel desea crear un sistema de atención en línea, donde los usuarios puedan pre-ordenar medicamentos para facilitar su recolección', '20', '10/17/2016', '11/26/2016', ''
+						
+EXEC SP_Crear_Propuesta_Subasta 2, '1', 35000, 'Knowledge is power, Guard it well!'
+EXEC SP_Crear_Propuesta_Subasta 2, '2', 55000, 'Sisters, we must go as Martyrs! We must go up in flames!'
+								
+EXEC SP_Aceptar_Subasta '2'
+
+EXEC SP_Insertar_Mensaje_Trabajo 'Por favor enviarme una propuesta inicial del proyecto', '', '10/17/2016', '2'
+EXEC SP_Insertar_Respuesta '1', 'Aquí adjunto la propuesta', '', '10/18/2016'
+
+EXEC SP_Insertar_Notificacion 'Felicidades! Usted ha ganado el proyecto Drpischel', '10/17/2016', '1'
+
+EXEC SP_Agregar_Al_Curso @IdEstudiante CHAR(100), @IdCurso INT, @Estado CHAR(1)
+	AS
+		INSERT INTO ESTUDIANTE_POR_CURSO (IdEstudiante, IdCurso, Estado)
+		VALUES (@IdEstudiante, @IdCurso, 'A')
+	GO
--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -4,7 +4,20 @@ IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'sp_insert_estu
 DROP PROCEDURE sp_insert_estudiante   
 GO
 
+CREATE PROCEDURE SP_Select_Cursos @UserId INT
+	AS
+		SELECT Curso, SUM(PuntajeBadge) AS Nota, EstadoCurso
+		FROM VIEW_CALIFICACIONES
+		WHERE IdEstudiante=@UserId AND EstadoBadge='O'
+		GROUP BY Curso, EstadoCurso
+	GO
 
+CREATE PROCEDURE SP_Nota_Poyecto @UserID INT, @ProjectId INT
+	AS
+		SELECT SUM(PuntajeBadge) AS Nota
+		FROM VIEW_CALIFICACIONES
+		WHERE IdEstudiante=@UserID AND EstadoBadge='O' AND IdProyecto=@ProjectId
+	GO
 
 
 CREATE PROCEDURE SP_Insertar_Estudiante   
@@ -27,16 +40,18 @@ CREATE PROCEDURE SP_Insertar_Profesor
 	@Id CHAR(100), @Contrasena CHAR(8), @Sal CHAR(20), @RepositorioArchivos CHAR(100), @CredencialDrive CHAR(100),
 
 	@NombreContacto CHAR(30), @ApellidoContacto CHAR(30), @Email CHAR(50), @Telefono CHAR(15), 
-	@FechaInscripcion DATE, @HorarioAtencion CHAR(15), @Pais CHAR(30), @Region CHAR(30)
+	@FechaInscripcion DATE, @HorarioAtencion CHAR(15), @Pais CHAR(30), @Region CHAR(30), @IdUniversidad INT
 		AS
-		INSERT INTO USUARIO (Id,Contrasena, Sal, RepositorioArchivos, CredencialDrive, Estado) 
-		VALUES (@Id, @Contrasena, @Sal, @RepositorioArchivos, @CredencialDrive, 'A')
+			INSERT INTO USUARIO (Id,Contrasena, Sal, RepositorioArchivos, CredencialDrive, Estado) 
+			VALUES (@Id, @Contrasena, @Sal, @RepositorioArchivos, @CredencialDrive, 'A')
 
-		INSERT INTO PROFESOR (Id, NombreContacto, ApellidoContacto, Email, Telefono, FechaInscripcion, HorarioAtencion, Pais,
-		Region)
-		VALUES (@Id, @NombreContacto, @ApellidoContacto, @Email, @Telefono, @FechaInscripcion, @HorarioAtencion, @Pais,
-		@Region)
+			INSERT INTO PROFESOR (Id, NombreContacto, ApellidoContacto, Email, Telefono, FechaInscripcion, HorarioAtencion, Pais,
+			Region)
+			VALUES (@Id, @NombreContacto, @ApellidoContacto, @Email, @Telefono, @FechaInscripcion, @HorarioAtencion, @Pais,
+			@Region)
 
+			INSERT INTO PROFESOR_POR_UNIVERSIDAD (IdProfesor, IdUniversidad, Estado)
+			VALUES (@Id, @IdUniversidad, 'A')
 		GO
 
 CREATE PROCEDURE SP_Insertar_Empresa
@@ -65,33 +80,155 @@ CREATE PROCEDURE SP_Insertar_Admin
 		VALUES (@Id, @Nombre, @ApellidoContacto) 
 	GO
 
-CREATE PROCEDURE SP_Insertar_Tecnologia
-@Id INT, @Nombre CHAR(30)
+
+
+CREATE PROCEDURE SP_Insertar_Tecnologia @Nombre CHAR(30)
 	AS
-		INSERT INTO TECNOLOGIA (Id, Nombre)
-		VALUES (@Id, @Nombre)
+		INSERT INTO TECNOLOGIA (Nombre, Estado)
+		VALUES (@Nombre, 'A')
 	GO
 
-CREATE PROCEDURE SP_Insertar_Universidad
-@Id INT, @Nombre CHAR(30)
+CREATE PROCEDURE SP_Insertar_Universidad @Nombre CHAR(30)
 	AS
-		INSERT INTO UNIVERSIDAD (Id, Nombre)
-		VALUES (@Id, @Nombre)
+		INSERT INTO UNIVERSIDAD (Nombre, Estado)
+		VALUES (@Nombre, 'A')
 	GO
 
-CREATE PROCEDURE SP_Select_Cursos
-@UserId INT
+
+CREATE PROCEDURE SP_Insertar_Curso @IdProfesor CHAR(100), @Nombre CHAR(30), @Codigo CHAR(10), @IdUniversidad INT
 	AS
-		SELECT Curso, SUM(PuntajeBadge) AS Nota, EstadoCurso
-		FROM VIEW_CALIFICACIONES
-		WHERE IdEstudiante=@UserId AND EstadoBadge='O'
-		GROUP BY Curso, EstadoCurso
+		DECLARE @IdCurso INT
+		INSERT INTO CURSO (Nombre, Codigo, Estado)
+		VALUES (@Nombre, @Codigo, 'A')
+
+		SELECT @IdCurso = @@IDENTITY
+		INSERT INTO CURSO_POR_PROFESOR (IdCurso, IdProfesor, Estado)
+		VALUES (@IdCurso, @IdProfesor, 'A')
+
+		INSERT INTO CURSO_POR_UNIVERSIDAD (IdCurso, IdUniversidad, Estado)
+		VALUES (@IdCurso, @IdUniversidad, 'A')
 	GO
 
-CREATE PROCEDURE SP_Nota_Poyecto
-@UserID INT, @ProjectId INT
+CREATE PROCEDURE SP_Insertar_Badge (@Nombre CHAR (30), @Puntaje TINYINT, @IdCurso INT) 
 	AS
-		SELECT SUM(PuntajeBadge) AS Nota
-		FROM VIEW_CALIFICACIONES
-		WHERE IdEstudiante=@UserID AND EstadoBadge='O' AND IdProyecto=@ProjectId
+		INSERT INTO BADGE (Nombre, Puntaje, IdCurso)
+		VALUES (@Nombre, @Puntaje, @IdCurso)
+
 	GO
+
+CREATE PROCEDURE SP_Otorgar_Badge (@IdBadge INT, @IdProyecto INT)
+	AS
+		INSERT INTO BADGE_POR_PROYECTO (IdBadge, IdProyecto, Estado)
+		VALUES (@IdBadge, @IdProyecto, 'A')
+	GO
+
+CREATE PROCEDURE SP_Insertar_Proyecto @IdEstudiante CHAR(100), @Nombre CHAR(30), @Problematica CHAR(100), @Descripcion CHAR(500), @IdCurso INT, @FechaInicio DATE, @FechaFinal DATE, @DocumentoAdicional CHAR(100), @NotaMinima INT 
+	AS
+		DECLARE @IdProyecto INT
+		INSERT INTO PROYECTO (Nombre, Problematica, Descripcion, IdCurso, FechaInicio, FechaFinal, DocumentoAdicional, NotaMinima, Estado)
+		VALUES (@Nombre,@Problematica, @Descripcion, @IdCurso, @FechaInicio, @FechaFinal, @DocumentoAdicional, @NotaMinima, 'P')
+
+		SELECT @IdProyecto = @@IDENTITY
+		INSERT INTO PROYECTO_POR_ESTUDIANTE (IdProyecto, IdEstudiante, Estado)
+		VALUES (@IdProyecto, @IdEstudiante, 'P')
+	GO
+
+CREATE PROCEDURE SP_Aceptar_Propuesta @IdProfesor CHAR(100), @IdPropuesta CHAR(1)
+	AS
+		UPDATE PROYECTO
+		SET Estado='A'
+		WHERE Id=@IdPropuesta
+
+		UPDATE PROYECTO_POR_ESTUDIANTE
+		SET Estado='A'
+		WHERE IdEstudiante=@IdPropuesta
+
+		INSERT INTO PROYECTO_POR_PROFESOR (IdProyecto, IdProfesor, Estado)
+		VALUES (@IdPropuesta, @IdProfesor, 'A')
+	GO
+
+CREATE PROCEDURE SP_Insertar_Trabajo @Nombre CHAR(30), @Descripcion CHAR(500), @IdEmpresa CHAR(100), @FechaInicio DATE, @FechaCierre DATE, @DocumentoAdicional CHAR(100)
+	AS
+		INSERT INTO TRABAJO (Nombre, Descripcion, IdEmpresa, FechaInicio, FechaCierre, DocumentoAdicional, Estado)
+		VALUES (@Nombre, @Descripcion, @IdEmpresa, @FechaInicio, @FechaCierre, @DocumentoAdicional, 'P')
+	GO
+
+CREATE PROCEDURE SP_Crear_Propuesta_Subasta @IdTrabajo INT, @IdEstudiante CHAR(100), @Monto INT, @Comentario CHAR(300)
+	AS
+		INSERT INTO TRABAJO_POR_ESTUDIANTE (IdTrabajo, IdEstudiante, Monto, Comentario, Estado)
+		VALUES (@IdTrabajo, @IdEstudiante, @Monto, @Comentario, 'P')
+	GO
+
+CREATE PROCEDURE SP_Aceptar_Subasta @IdSubasta CHAR(1), @IdEstudiante CHAR(100)
+	AS
+		UPDATE TRABAJO_POR_ESTUDIANTE
+		SET Estado='X'
+		WHERE IdTrabajo=@IdSubasta
+
+		UPDATE TRABAJO_POR_ESTUDIANTE
+		SET Estado='A'
+		WHERE IdEstudiante=@IdEstudiante AND IdTrabajo=@IdSubasta
+		
+		UPDATE TRABAJO
+		SET Estado='A'
+		WHERE Id=@IdSubasta
+	GO
+
+CREATE PROCEDURE SP_Insertar_Mensaje_Proyecto @Contenido CHAR(500), @Adjunto CHAR(500), @Fecha DATETIME, @IdProyecto INT
+	AS
+		DECLARE @IdMensaje BIGINT
+		INSERT INTO MENSAJE (Contenido, Adjunto, Fecha)
+		VALUES (@Contenido, @Adjunto, @Fecha)
+		
+		SELECT @IdMensaje = @@IDENTITY
+		INSERT INTO MENSAJE_POR_PROYECTO (IdMensaje, IdProyecto, Estado)
+		VALUES (@IdMensaje,  @IdProyecto, 'A')
+	GO
+
+CREATE PROCEDURE SP_Insertar_Mensaje_Trabajo @Contenido CHAR(500), @Adjunto CHAR(500), @Fecha DATETIME, @IdTrabajo INT
+	AS
+		DECLARE @IdMensaje BIGINT
+		INSERT INTO MENSAJE (Contenido, Adjunto, Fecha)
+		VALUES (@Contenido, @Adjunto, @Fecha)
+		
+		SELECT @IdMensaje = @@IDENTITY
+		INSERT INTO MENSAJE_POR_TRABAJO (IdMensaje, IdTrabajo, Estado)
+		VALUES (@IdMensaje,  @IdTrabajo, 'A')
+	GO
+
+CREATE PROCEDURE SP_Insertar_Respuesta @MensajeRaiz BIGINT, @Contenido CHAR(500), @Adjunto CHAR(500), @Fecha DATETIME
+	AS
+		INSERT INTO RESPUESTA (MensajeRaiz, Contenido, Adjunto, Fecha)
+		VALUES (@MensajeRaiz, @Contenido, @Adjunto, @Fecha)
+	GO
+
+/*Automatizar?*/
+CREATE PROCEDURE SP_Insertar_Notificacion @Contenido CHAR(500), @Fecha DATETIME, @UserId CHAR(100)
+	AS
+		INSERT INTO NOTIFICACION (Contenido, Fecha, UserId, Estado)
+		VALUES (@Contenido, @Fecha, @UserId, 'A')
+	GO
+
+CREATE PROCEDURE SP_Asignar_Tecnologia @IdProyecto INT, @IdTecnologia INT
+	AS
+		INSERT INTO TECNOLOGIA_POR_PROYECTO (IdProyecto, IdTecnologia, Estado)
+		VALUES (@IdProyecto, @IdTecnologia, 'A')
+	GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
