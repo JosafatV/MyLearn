57f96881682ab2cb9e54209a44bf5aa61db8546d
JosafatV <josav09@gmail.com>
2016-11-19 19:49:05 -0600

    Significado de los estados
    v1 del Significado de los estados estado en la base de datos

---
+++ Significado de los estado.sql
@@ -0,0 +1,17 @@
+Significado de los estado
+
+
+A: Activo(Trabajo, Proyecto), Aceptado(Oferta, Propuesta), Alardeado (Badge)
+
+E: Exito (Trabajo, Proyecto), Aprobado (Curso)
+F: Fracaso (Trabajo, Proyecto), Reprobado (Curso)
+
+I: Inactivo (Borrado lógico)
+
+O: Obtenido (Badge)
+P: Pendiente(Trabajo, Proyecto), Propuesto(Oferta, Propuesta), Posible (Badge)
+
+
+X: Terminado(Trabajo, Proyecto), Rechazado(Oferta, Propuesta)
+
+
