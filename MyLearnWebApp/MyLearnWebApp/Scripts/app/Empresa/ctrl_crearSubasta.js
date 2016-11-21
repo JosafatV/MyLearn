df3bc6b02c055ff7dc7856274b3f5952f9320549
Giovanni Villalobos <gjosue253@gmail.com>
2016-11-21 03:16:12 -0600

    Crear Subasta

--- MyLearnWebApp/MyLearnWebApp/Scripts/app/Empresa/ctrl_crearSubasta.js
+++ MyLearnWebApp/MyLearnWebApp/Scripts/app/Empresa/ctrl_crearSubasta.js
@@ -5,4 +5,22 @@
             $location.path('/MyLearn/Empresa/Perfil');
         };
 
+        $scope.js_crearSubasta = {
+            "Nombre": "",
+            "Descripcion": "",
+            "IdEmpresa": fct_User.getId(),
+            "FechaCierre": "",
+            "PresupuestoBase": ""
+        }
+
+        $scope.set_newSubasta = function () {
+
+            fct_MyLearn_API_Client.save({ type: 'Subastas' }, $scope.js_crearSubasta).$promise.then(function (data) {
+                alert(angular.toJson(data));
+            });
+
+        };
+
+
+
     }]);
