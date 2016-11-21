6930f0a150e6b3cd292032e106bd69aab1571c97
Sebas95 <sgonzalezquesada1@gmail.com>
2016-11-21 17:07:15 -0600

    update (antes de hacer area de trabajo empresa-estudiante)

---
+++ MyLearnApi/MyLearnApi/BusinessLogic/clsAreaDeTrabajoLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using MyLearnApi.Models;
+
+
+namespace MyLearnApi.BusinessLogic
+{
+    public class clsAreaDeTrabajoLogic
+    {
+    }
+}
