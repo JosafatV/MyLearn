3847738c2330e73931c88783af60a7ffa6a50fa4
JosafatV <josav09@gmail.com>
2016-11-21 13:18:46 -0600

    Revert "más trabajos, más ofertas"
    This reverts commit 7cff5abe402e125461abf6ac677afcb34bdd9e4a.

--- Significado de los estado.sql
+++ Significado de los estado.sql
@@ -1,17 +1,17 @@
 Significado de los estado
 
 
-A: Activo(Trabajo, Proyecto), Aceptado(Oferta, Propuesta), Alardeado (Badge), Inscrito (Est por Curso)
+A: Activo(Trabajo, Proyecto), Aceptado(Oferta, Propuesta), Alardeado (Badge)
 
-E: Exito (Trabajo, Proyecto), Aprobado (Est por Curso)
-F: Fracaso (Trabajo, Proyecto), Reprobado (Est por Curso)
+E: Exito (Trabajo, Proyecto), Aprobado (Curso)
+F: Fracaso (Trabajo, Proyecto), Reprobado (Curso)
 
 I: Inactivo (Borrado lógico)
 
 O: Obtenido (Badge)
-P: Pendiente(Trabajo, Proyecto), Propuesto(Oferta, Propuesta), Posible (Badge), Disponible (Curso)
+P: Pendiente(Trabajo, Proyecto), Propuesto(Oferta, Propuesta), Posible (Badge)
 
 
-X: Terminado(Trabajo, Proyecto, Curso), Rechazado(Oferta, Propuesta)
+X: Terminado(Trabajo, Proyecto), Rechazado(Oferta, Propuesta)
 
 
