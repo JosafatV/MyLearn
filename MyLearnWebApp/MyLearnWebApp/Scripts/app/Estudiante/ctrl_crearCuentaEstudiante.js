27da107ed2c2dcda83762cd3c2a72504c4153f08
Giovanni Villalobos <gjosue253@gmail.com>
2016-11-26 16:58:02 -0600

    Cambios para authenticar con drive

--- MyLearnWebApp/MyLearnWebApp/Scripts/app/Estudiante/ctrl_crearCuentaEstudiante.js
+++ MyLearnWebApp/MyLearnWebApp/Scripts/app/Estudiante/ctrl_crearCuentaEstudiante.js
@@ -1,6 +1,9 @@
 angular.module('mod_MyLearn').controller('ctrl_crearCuentaEstudiante', ['$q', '$scope', '$routeParams', '$location', 'ModalService', 'fct_MyLearn_API_Client', 'twitterService',
     function ($q, $scope, $routeParams, $location, ModalService, fct_MyLearn_API_Client, twitterService) {
 
+        var access_token = "";
+        var refresh_token = "";
+        var client_id = "";      
         $scope.universidadSelected = "";
         $scope.str_paisSelected = "";
         $scope.ls_tecnologiasSelect = [];
@@ -21,6 +24,11 @@ angular.module('mod_MyLearn').controller('ctrl_crearCuentaEstudiante', ['$q', '$
             CredencialDrive: "",
             IdUniversidad: ""
         }
+
+        getClientId();
+
+        OAuth.initialize('CgKcLvAzYP_vq69R1HNBPtTne_g');
+        OAuth.create('google_drive');
 
         $scope.js_crearTecnologia = {
             IdTecnologia: "",
@@ -64,6 +72,64 @@ angular.module('mod_MyLearn').controller('ctrl_crearCuentaEstudiante', ['$q', '$
         $scope.do_doleteSelected = function (tec) {
             $scope.ls_tecnologias.push(tec);
             $scope.ls_tecnologiasSelect.splice($scope.ls_tecnologiasSelect.indexOf(tec), 1);
+        };
+
+
+        /*
+        * Esta es la funcion encargada de recibir el clientId
+        */
+        function getClientId() {
+            fct_MyLearn_API_Client.get({ type: 'ClientSecret' }).$promise.then(function (data) {
+                client_id = data.client_id;
+            });
+        };
+
+        /*
+        * Esta es la funcion encargada de conectar con Drive
+        */
+
+        $scope.testDrive = function () {
+            OAuth.clearCache();
+            OAuth.popup('google_drive', { cache: false }).done(function (result) {
+                console.log(result);
+                access_token = result.access_token;
+                refresh_token = result.refresh_token;
+            })
+        };
+
+
+        /*
+        * Función encargada de enviar las credential 
+        */
+
+        function set_sendCredentials(id) {
+            fct_MyLearn_API_Client.save({ type: 'DriveCredentials' }, {
+                "UserId": id,
+                "AccessToken": access_token,
+                "RefreshToken": refresh_token
+            }).$promise.then(function (data) {
+                alert(angular.toJson(data));
+            });
+        };
+
+        /*
+        * Esta es la funcion encargada de enviar el archivo a la base de datos
+        */
+
+        function sendFile(){
+            var file = $scope.myFile;
+
+            console.log('file is ');
+            console.dir(file);
+
+            var uploadUrl = "/fileUpload";
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
+
+        $scope.getFile = function () {
+            var file = $scope.myFile;
+            console.log('file is ');
+            console.info(file);
         };
 
         $scope.do_agregueTec = function (tec) {
---
+++ MyLearnWebApp/MyLearnWebApp/Scripts/app/services.js
@@ -0,0 +1,7 @@
+﻿angular.module('mod_MyLearn').service('fileUpload', ['fct_MyLearn_API_Client', function ($http) {
+    this.uploadFileToUrl = function (file, uploadUrl) {
+        var fd = new FormData();
+        fd.append('file', file);
+
+    }
+}]);
