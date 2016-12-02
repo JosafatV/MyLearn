5cf89c41770f9e38cc9541680c702819775a54ca
Giovanni Villalobos <gjosue253@gmail.com>
2016-12-01 18:59:39 -0600

    Cerrar Sesion service

---
+++ MyLearnWebApp/MyLearnWebApp/Scripts/app/Services/srcv_cerrarSesion.js
@@ -0,0 +1,5 @@
+﻿angular.module('mod_MyLearn').service('cerrarSesion', ['$http', '$q', function ($http, $q) {
+    this.cerrarSesion = function () {
+        $location.path('/MyLearn/Empresa/CerrarSesion');
+    }
+}]);
