bc5c0a50c952a6ee8570b124e8bce9172ab38b44
Sebas95 <sgonzalezquesada1@gmail.com>
2016-12-02 22:43:09 -0600

    update unit tests

--- MyLearnApi/MyLearnTests/UnitTest1.cs
+++ MyLearnApi/MyLearnTests/UnitTest1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using MyLearnApi.BusinessLogic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLearnApi.Models;
 using MyLearnApi.BusinessLogic.UserAccounts;
 
 namespace MyLearnApiTests
@@ -10,7 +12,7 @@ namespace MyLearnApiTests
     public class UnitTest1
     {
 
-
+        //verifica que se encripte una contraseña de manera correcta
         [TestMethod]
         public void Test_Encription()
         {
@@ -22,6 +24,7 @@ namespace MyLearnApiTests
             Assert.AreEqual(myHash, BCrypt.HashPassword(myPassword, Sal));
         }
 
+        //verifica que la revisión de las contraseñas funcione de manera correcta
         [TestMethod]
         public void Test_CheckPassword()
         {
@@ -34,6 +37,7 @@ namespace MyLearnApiTests
             Assert.AreEqual(false, BCrypt.CheckPassword(wrongPassword, myHash));
         }
 
+        //verifica que se generan sales diferentes 
         [TestMethod]
         public void Test_SaltGeneration()
         {
@@ -47,6 +51,7 @@ namespace MyLearnApiTests
             Assert.AreNotEqual(Sal3, Sal4);
         }
 
+        //revisa los métodos de análisis de visual studio
         [TestMethod]
         public void Test_idProfesor()
         {
@@ -54,7 +59,8 @@ namespace MyLearnApiTests
             Assert.AreSame("13", idProfesor);
             Assert.AreEqual("13", idProfesor);
         }
-        /*
+
+        //prueba que se realice el twitt de manera correcta
         [TestMethod]
         public void Test_twittSubasta()
         {
@@ -63,10 +69,11 @@ namespace MyLearnApiTests
             string Twitter = repo.twittSubasta("Unit Test");
 
             Assert.IsNotNull(Twitter);
-        }*/
+        }
 
+        //revisa el método de paginación
         [TestMethod]
-        public void Test_paginarLista20()
+        public void Test_PaginarLista20()
         {
             int index = 0;
             byte offset = 20;
@@ -100,11 +107,15 @@ namespace MyLearnApiTests
             Assert.AreEqual(24, chapters.Count);
             Assert.AreEqual(20, expectedlist.Count);
             Assert.AreEqual(20, list.Count);
-            //Assert.AreEqual(expectedlist, list);
+
+            List<object> lista = expectedlist.Except(list).ToList();
+            List<object> listb = list.Except(expectedlist).ToList();
+            Assert.AreEqual(lista.Count, listb.Count);
         }
 
+        //revisa el método de paginación para diferentes casos específicos
         [TestMethod]
-        public void Test_paginarLista7()
+        public void Test_PaginarLista7()
         {
             int index = 0;
             int index2 = 1;
@@ -151,15 +162,76 @@ namespace MyLearnApiTests
             Assert.AreEqual(7, list.Count);
             Assert.AreEqual(7, list2.Count);
             Assert.AreEqual(3, list3.Count);
-            //Assert.AreEqual(expectedlist, list);
-            //Assert.AreEqual(expectedlist2, list2);
-            //Assert.AreEqual(expectedlist3, list3);
+
+            List<object> lista = expectedlist.Except(list).ToList();
+            List<object> listb = list.Except(expectedlist).ToList();
+            Assert.AreEqual(lista.Count, listb.Count);
+
+            lista = expectedlist2.Except(list2).ToList();
+            listb = list2.Except(expectedlist2).ToList();
+            Assert.AreEqual(lista.Count, listb.Count);
+
+            lista = expectedlist3.Except(list3).ToList();
+            listb = list3.Except(expectedlist3).ToList();
+            Assert.AreEqual(lista.Count, listb.Count);
         }
 
+        //verifica que los badge se cuenten de manera correcta
         [TestMethod]
-        public void Test()
+        public void Test_ContarBadges()
         {
+            BADGE bdge = new BADGE();
+            bdge.Id = 1;
+            bdge.IdCurso = 3;
+            bdge.Nombre = "testbadge1";
+            bdge.Puntaje = 25;
 
+            BADGE bdge2 = new BADGE();
+            bdge2.Id = 2;
+            bdge2.IdCurso = 3;
+            bdge2.Nombre = "testbadge2";
+            bdge2.Puntaje = 25;
+
+            BADGE bdge3 = new BADGE();
+            bdge3.Id = 3;
+            bdge3.IdCurso = 3;
+            bdge3.Nombre = "testbadge3";
+            bdge3.Puntaje = 50;
+
+            List<BADGE> listBadge = new List<BADGE>();
+            listBadge.Add(bdge);
+            listBadge.Add(bdge2);
+            listBadge.Add(bdge3);
+
+            Byte b25 = 25;
+            Byte b50 = 50;
+
+            Assert.AreEqual(b25, bdge.Puntaje);
+            Assert.AreEqual(b25, bdge2.Puntaje);
+            Assert.AreEqual(b50, bdge3.Puntaje);
+
+            clsCursosLogic curso = new clsCursosLogic();
+            Assert.IsTrue(curso.isTotalPuntajeValido(listBadge));
+
+        }
+
+        //verifica quese agrega una respuesta de manera correcta
+        [TestMethod]
+        public void Test_AgregarRespuesta()
+        {
+            MENSAJE mns = new MENSAJE();
+            mns.Id = 1;
+            mns.Contenido = "Unit Test sent as message";
+            mns.NombreEmisor = "The Emperor of Mankind";
+
+            RESPUESTA res = new RESPUESTA();
+            res.Id = 1;
+            res.MensajeRaiz = 1;
+            res.NombreEmisor = "Malcador The Sigilite";
+            
+            mns.RESPUESTA.Add(res);
+
+            Assert.AreEqual(res.Contenido, mns.RESPUESTA.First<RESPUESTA>().Contenido);
         }
     }
 }
