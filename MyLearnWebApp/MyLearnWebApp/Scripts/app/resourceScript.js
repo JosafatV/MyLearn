6dfed4fb9eae92e4c9ecc1fd29b6587e7d61ca73
Giovanni Villalobos <gjosue253@gmail.com>
2016-12-03 10:44:18 -0600

    Ip para laimi

--- MyLearnWebApp/MyLearnWebApp/Scripts/app/resourceScript.js
+++ MyLearnWebApp/MyLearnWebApp/Scripts/app/resourceScript.js
@@ -1,5 +1,5 @@
 ﻿//Var used to save the URL 
-var urlGeneric = 'http://192.168.1.10';
+var urlGeneric = 'http://172.19.13.20';
 
 //This script is the resource that is used to connect to the web Api od DrPhischel
 angular.module('mod_MyLearn').factory('fct_MyLearn_API_Client', function ($resource) {
